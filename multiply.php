c8c0ab5bd133cd1fa33a677ceab239b6c0871747
Steven Anagnostos <sjanagnostos@email.neit.edu>
2017-10-02 10:56:54 -0400

    Table structure

--- multiply.php
+++ multiply.php
@@ -1,3 +1,16 @@
+<?php
+
+$table = "<table>"; //empty table
+for($rows=0; $rows<5; $rows++){
+    $table .= "\t<tr>";
+    $table .= "</tr>\n";
+}
+
+$table .= "</table>";
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +18,6 @@
     <title>Title</title>
 </head>
 <body>
-
+<?php echo $table; ?>
 </body>
 </html>
